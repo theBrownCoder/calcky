b883f315f42b32205ebc31121f78f258cb9ac4bf
dameonwilson <thebrowncoder@gmail.com>
2020-02-12 02:16:59 -0500

    Added num & operator variables; Reduced functions.

--- scripts/main.js
+++ scripts/main.js
@@ -1,44 +1,60 @@
-const nine = {
-    eventcode: 'Numpad9',
-    padcode: 'Digit9'
-}
-function add(...args) {
-  let input = [...args];
-  let sum = 0;
+'use strict';
 
-  for (let x = 0; x < input.length; x++) {
-    sum += input[x];
+// Numbers
+const zero = '0';
+const one = '1';
+const two = '2';
+const three = '3';
+const four = '4';
+const five = '5';
+const six = '6';
+const seven = '7';
+const eight = '8';
+const nine = '9';
+const ten = '10';
+
+// Operators
+const addition = '+';
+const subtraction = '-';
+const multiplication = '*';
+const division = '/';
+
+// Addition
+function add(num1, num2) {
+  return num1 + num2;
+}
+
+// Subtraction
+function subtract(num1, num2) {
+  return num1 - num2;
+}
+
+// Multiplication
+function multiply(num1, num2) {
+  return num1 * num2;
+}
+
+// Division
+function divide(num1, num2) {
+  return num1 / num2;
+}
+
+// Operation
+function operate(op, num1, num2) {
+  switch (op) {
+    case '+':
+      return add(num1, num2);
+      break;
+    case '-':
+      return subtract(num1, num2);
+      break;
+    case '*':
+      return multiply(num1, num2);
+      break;
+    case '/':
+      return divide(num1, num2);
+      break;
+    default:
+      console.log('Error. Something went wrong.');
   }
-  return sum;
-}
-
-function subtract(...args) {
-  let input = [...args];
-  let sum = 0;
-  for (let s = 0; s < input.length; s++) {
-    sum -= input[s];
-  }
-  return sum;
-}
-
-function multiply(...args) {
-  let input = [...args];
-  let sum = 1;
-  for (let m = 0; m < input.length; m++) {
-    sum *= input[m];
-  }
-  return sum;
-}
-
-function divide(...args) {
-  let input = [...args];
-  let sum = input[0];
-  for (let d = 1; d < input.length; d++) {
-    sum /= input[d];
-  }
-  return sum;
-}
-
-function operate(op, num1, num2){
-
 }
