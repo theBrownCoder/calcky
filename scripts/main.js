093fa7e42026e08c65915e20b6d3f0d9ab7206bc
dameonwilson <thebrowncoder@gmail.com>
2020-02-18 02:21:18 -0500

    Removed # array,  renamed vars, & edited functions

--- scripts/main.js
+++ scripts/main.js
@@ -1,25 +1,57 @@
 'use strict';
 
-// Numbers
-const zero = '0';
-const one = '1';
-const two = '2';
-const three = '3';
-const four = '4';
-const five = '5';
-const six = '6';
-const seven = '7';
-const eight = '8';
-const nine = '9';
-const ten = '10';
-
-// Operators
 const addition = '+';
+const division = '/';
 const subtraction = '-';
 const multiplication = '*';
-const division = '/';
-const equals = '=';
-const equals2 = 'Enter';
+const equal = '=';
+
+let firstNum = '';
+let secondNum = '';
+let sign = '';
+let usrInput = '';
+let op = '';
+
+const display = document.querySelector('#input-span');
+const clear = document.querySelector(newFunction());
+const equals = document.querySelector('#equals-key');
+const digits = document.querySelectorAll('.numpad-button');
+
+clear.addEventListener('click', e => {
+  display.textContent = '0';
+  usrInput = '';
+  firstNum = '';
+  secondNum = '';
+});
+equals.addEventListener('click', e => {
+  operate(parseInt(firstNum), checkOperator(op), parseInt(secondNum));
+});
+
+digits.forEach(button => {
+  button.addEventListener('click', e => {
+    display.textContent = '';
+    usrInput = `${button.textContent}`;
+    if (firstNum.length > 0) {
+      if (isNaN(parseInt(usrInput))) {
+        op = usrInput;
+        console.log(op);
+        display.textContent += op;
+      } else {
+        secondNum = usrInput;
+        console.log(secondNum);
+        display.textContent += secondNum;
+      }
+    } else {
+      firstNum = usrInput;
+      console.log(firstNum);
+      display.textContent += firstNum;
+    }
+  });
+});
+
+function newFunction() {
+  return '#data-input-clear-key';
+}
 
 // Addition
 function add(num1, num2) {
@@ -32,7 +64,7 @@ function subtract(num1, num2) {
 }
 
 // Multiplication
-function multiply(num1, num2) {
+function mult(num1, num2) {
   return num1 * num2;
 }
 
@@ -42,21 +74,45 @@ function divide(num1, num2) {
 }
 
 // Operation
-function operate(op, num1, num2) {
+function operate(num1, op, num2) {
   switch (op) {
-    case '+':
-      return add(num1, num2);
+    case addition:
+      display.textContent = String(add(num1, num2));
       break;
-    case '-':
-      return subtract(num1, num2);
+
+    case subtraction:
+      display.textContent = String(subtract(num1, num2));
       break;
-    case '*':
-      return multiply(num1, num2);
+
+    case multiplication:
+      display.textContent = String(mult(num1, num2));
       break;
-    case '/':
-      return divide(num1, num2);
+
+    case division:
+      display.textContent = String(divide(num1, num2));
       break;
+
     default:
       console.log('Error. Something went wrong.');
+  }
+}
+function checkOperator(input) {
+  switch (input) {
+    case addition:
+      return '+';
+
+    case subtraction:
+      return '-';
+
+    case multiplication:
+      return '*';
+
+    case division:
+      return '/';
+
+    case equal:
+      return '=';
+    default:
+      console.log('Operator could not be verified.');
   }
 }
